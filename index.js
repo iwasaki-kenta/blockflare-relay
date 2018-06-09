1496edf52567dee3ba9aa6b18ade136497ce1b41
Kenta Iwasaki <dranithix@gmail.com>
2018-06-10 03:39:31 +0800

    Created web panel.

--- index.js
+++ index.js
@@ -1,26 +1,92 @@
 import ecc from 'eosjs-ecc'
+import Eos from 'eosjs'
 
-const data = {
-  request: {
-    code: "GET",
-    username: "Hi",
-    password: "secretpassword"
-  },
-  nonce: 0
+const eos = Eos({
+  keyProvider: ['5Ju8dQQheWe73SMjKG4oaPiVu3AVhyFxsRYHMPVGpeAz1nnw2L6'],
+  sign: true,
+})
+
+class Relayer {
+  difficulty = 4
+
+  constructor() {
+  }
+
+  generateDifficultyPrefix() {
+    let difficultyString = ''
+    for (let i = 0; i < this.difficulty; i++) difficultyString += '0'
+    return difficultyString
+  }
+
+  delegateProof(data) {
+    const prefix = this.generateDifficultyPrefix()
+
+    let nonce = 0
+    let serialized = data + nonce
+    let hash = ecc.sha256(serialized)
+
+    while (hash.slice(0, this.difficulty) !== prefix) {
+      nonce = Math.random() * 100000000000
+      serialized = data + nonce.toString()
+      hash = ecc.sha256(serialized)
+    }
+
+    return {hash, nonce: nonce.toString()}
+  }
 }
 
-let serialized = JSON.stringify(data)
-let hash = ecc.sha256(serialized)
+(async () => {
+  const data = 'this_is_test_data'
 
-const difficulty = 4
+  const relayer = new Relayer()
+  const before = new Date().getTime()
 
-let difficultyString = ""
-for (let i = 0; i < difficulty; i++) difficultyString += "0"
+  const proof = relayer.delegateProof(data)
 
-while (hash.slice(0, difficulty) !== difficultyString) {
-  data.nonce = Math.random() * 100000000000
-  serialized = JSON.stringify(data)
-  hash = ecc.sha256(serialized)
-}
+  const contract = await eos.contract('blockflare')
 
-console.log(hash)
+  setInterval(async () => {
+    const accountIndex = 0;
+    const account = (await eos.getTableRows(true, 'blockflare', 'blockflare', 'ledger')).rows[accountIndex]
+    console.log(`Your account: ${account.owner}`);
+
+    const relay = (await eos.getTableRows(true, 'blockflare', 'blockflare', 'reque', 0, account.relaying, 1000)).rows.filter(r => r.id === account.relaying)[0]
+    if (relay) {
+      await contract.respond({relayer: "blockflare", response: "It's a response!"}, {authorization: ['blockflare']})
+
+      console.log(`Routed tx. ID ${relay.id} through your host ${account.relayAddress} to API located at "${relay.url}" w/ neighbouring nodes: [${relay.relayers.join(", ")}].`)
+    }
+    console.log()
+  }, 1000)
+
+  // let res
+  //
+  // try {
+  //   res = await contract.newendpoint({creator: 'blockflare', url: 'google.com'}, {authorization: ['blockflare']})
+  // } catch (error) {
+  //   res = await contract.delendpoint({url: 'google.com'}, {authorization: ['blockflare']})
+  //   res = await contract.newendpoint({creator: 'blockflare', url: 'google.com'}, {authorization: ['blockflare']})
+  //
+  // }
+  //
+  // try {
+  //   res = await contract.request({
+  //     sender: 'blockflare',
+  //     url: 'google.com',
+  //     message: data,
+  //     nonce: proof.nonce,
+  //     proof: proof.hash
+  //   }, {authorization: ['blockflare']})
+  //   console.log(res)
+  // } catch (error) {
+  //   console.log(error)
+  // }
+  //
+  // try {
+  //   res = await contract.delendpoint({url: 'google.com'}, {authorization: ['blockflare']})
+  // } catch (error) {}
+  //
+  // console.log(await eos.getKeyAccounts("EOS5cm3H9RX5F1ENbhch6rBnjGeVPb6VYF8VktYJcsxFzVRBfQS5P"))
+
+// eos.getTableRows(true, 'blockflare', 'blockflare', 'relayers', 0, 0, 1000).then(data => console.log(data))
+})()
---
+++ web/src/App.vue
@@ -0,0 +1,291 @@
+<template>
+    <div style="width: 100%; height: 100%; ">
+        <div id="container">
+            <div class="grid-header" style="margin-bottom: 0.1em;">
+                <div style="display: flex; align-items: center; height: 100%;">
+                    <h1>[blockflare.]</h1>
+                    <div class="space-partners" style="display: flex; flex-direction: row;">
+                        <div style="display: flex; flex-direction: column; text-align: left;">
+                            <span class="title">Account</span>
+                            <span>{{account && account.owner || "Loading..."}}</span>
+                        </div>
+
+                        <div style="display: flex; flex-direction: column; text-align: left;">
+                            <span class="title">Relay Nodes</span>
+                            <span>[{{connected.join(', ')}}]</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid-login" style="width: 1200px; height: 100%;">
+                <div style="display: flex; height: 100%; flex-direction: row;">
+                    <div style="padding: 2em; width: 350px;">
+                        <h2>Please login.</h2>
+
+                        <br/>
+
+                        <div class="form">
+                            <input type="text" placeholder="Username." v-model="form.username"/>
+                            <input type="password" placeholder="Password." v-model="form.password"/>
+                            <input type="button" value="Login" style="width: 108.75%;" @click="relay"
+                                   :disabled="mining"/>
+                        </div>
+
+                        <br/>
+
+                        <div style="width: 108.75%; height: 150px; background-color: white;">
+                            <div style="padding: 2em;">
+                                <span style="white-space: pre; font-family: monospace; font-size: 1.25em; font-weight: 600;">{{JSON.stringify(form, null, 4)}}</span>
+                            </div>
+                        </div>
+
+                        <br/>
+                    </div>
+
+                    <div style="padding: 2em;">
+                        <div style="height: 150px; background-color: white;">
+                            <div style="padding: 1em; width: 100%; height: 150px;">
+                                <b style="white-space: pre; font-family: monospace; font-size: 1em; font-weight: 600; overflow-wrap: break-word; word-break: break-all;">{{JSON.stringify(mine,
+                                    null, 4)}}</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid-log" style="width: 600px;">
+                <div style="display: flex; width: 100%; height: 100%; overflow-y: scroll;">
+                    <ul class="space-list" style="height: 700px; max-height: 700px; padding: 2em; word-wrap: break-word; overflow-wrap: break-word; overflow: auto;">
+                        <li v-for="log in logs">
+                            <code style="font-size: 1.5em;">{{log}}</code>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="grid-footer" style="margin-top: 1.25em; height: 80px;">
+                <div style="display: flex; align-items: center; height: 100%;">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+  import Eos from 'eosjs'
+  import ecc from 'eosjs-ecc'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        contract: null,
+        logs: [],
+        connected: ['localhost:3000', 'localhost:3001', 'localhost:3002'],
+        form: {
+          username: '',
+          password: ''
+        },
+        mine: {
+          difficulty: 1,
+          proof_of_work: null,
+          nonce: 0
+        },
+        mining: null,
+        account: {
+
+        }
+      }
+    },
+    components: {},
+    async mounted() {
+      this.eos = Eos({
+        keyProvider: ['5Ju8dQQheWe73SMjKG4oaPiVu3AVhyFxsRYHMPVGpeAz1nnw2L6'],
+        sign: true,
+      })
+
+      this.contract = await this.eos.contract('blockflare')
+
+      setInterval(async () => {
+        this.account = (await this.eos.getTableRows(true, 'blockflare', 'blockflare', 'ledger')).rows[0]
+        console.log(this.account)
+      }, 1000)
+    },
+    methods: {
+      relay() {
+        this.delegateProof(JSON.stringify(this.form))
+      },
+      generateDifficultyPrefix() {
+        let difficultyString = ''
+        for (let i = 0; i < this.mine.difficulty; i++) difficultyString += '0'
+        return difficultyString
+      },
+      log(message) {
+        this.logs.push(`[${new Date().getHours()}:${new Date().getMinutes()} ${new Date().getHours() > 12 ? "PM" : "AM"}] ${message}`)
+      },
+      delegateProof(data) {
+        if (!this.mining) {
+          const prefix = this.generateDifficultyPrefix()
+
+          let nonce = 0
+          let serialized = data + nonce
+          let proof_of_work = ecc.sha256(serialized)
+
+          this.mining = setInterval(async () => {
+            if (proof_of_work.slice(0, this.mine.difficulty) !== prefix) {
+              nonce = Math.random() * 100000000000
+              serialized = data + nonce.toString()
+              proof_of_work = ecc.sha256(serialized)
+            } else {
+              clearInterval(this.mining)
+
+              this.log(`Requesting for login verification w/ proof-of-work "${proof_of_work}"...`)
+
+              let res = await this.contract.request({
+                sender: 'blockflare',
+                url: 'google.com',
+                message: data,
+                nonce: nonce.toString(),
+                proof: proof_of_work
+              }, {authorization: ['blockflare']})
+
+              this.log(`Transaction signed and broadcasted on block ${res.transaction.transaction.ref_block_num}.`)
+              console.log(res)
+
+              const relay = 'localhost:3000'
+
+              res = await this.contract.assign({
+                relayer: 'blockflare',
+                relayAddress: relay
+              }, {authorization: ['blockflare']})
+
+              this.log(`Reached relay(s) ${this.connected.join(", ")}. Routing API request...`)
+
+              console.log(res)
+
+              this.mining = null
+            }
+
+            this.mine.proof_of_work = proof_of_work
+            this.mine.serialized = serialized
+            this.mine.nonce = nonce.toString()
+          }, 15)
+
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+    html {
+        width: 100%;
+        height: 100%;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        width: 100%;
+        height: 100%;
+
+        font-family: 'monospace', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        color: #2c3e50;
+    }
+
+    #container {
+        display: grid;
+        grid-template-areas: 'header header header header header header' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'login login login login log log' 'footer footer footer footer footer footer' 'footer footer footer footer footer footer';
+    }
+
+    #container > div {
+        background-color: #eaeaea;
+        margin: 0.5em;
+    }
+
+    .grid-header {
+        grid-area: header;
+        text-align: center;
+        padding-left: 1em;
+        background-color: white !important;
+    }
+
+    .grid-login {
+        grid-area: login;
+        height: 100%;
+    }
+
+    .grid-log {
+        grid-area: log;
+        height: 100%;
+        background-color: black !important;
+        color: lightgreen;
+    }
+
+    .grid-footer {
+        grid-area: footer;
+    }
+
+    #container {
+        width: 100%;
+        height: 100%;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        margin-bottom: 0.5em;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    code {
+        font-size: 2em;
+    }
+
+    h1 {
+        font-size: 3em;
+        margin-right: 1em;
+    }
+
+    input {
+        padding: 0.5em;
+        font-size: 2em;
+        font-family: monospace;
+        width: 100%;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+
+    }
+
+    .form > :not(:last-child) {
+        margin-bottom: 0.5em;
+    }
+
+    .space-partners > :not(:last-child) {
+        margin-right: 3em;
+    }
+
+    .title {
+        font-weight: 600;
+        font-size: 1.5em;
+    }
+
+    .space-list > :not(:last-child) {
+        margin-bottom: 1.25em;
+    }
+</style>
