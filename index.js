4d157279259cb9aef87032b5f0f55b848614955f
Kenta Iwasaki <dranithix@gmail.com>
2018-06-09 12:01:48 +0800

    Added in difficulty to proof of work.

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const data = {
 let serialized = JSON.stringify(data)
 let hash = ecc.sha256(serialized)
 
-while (hash[0] !== '0') {
+const difficulty = 4
+
+let difficultyString = ""
+for (let i = 0; i < difficulty; i++) difficultyString += "0"
+
+while (hash.slice(0, difficulty) !== difficultyString) {
   data.nonce = Math.random() * 100000000000
   serialized = JSON.stringify(data)
   hash = ecc.sha256(serialized)
